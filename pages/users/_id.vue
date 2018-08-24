5b06d4aeb6ab102b87aff357de7f3b41e68f7cdb
Rhio Kim <rhio.kim@gmail.com>
2018-08-25 00:46:53 +0900

    feat: add custom routing & async data fetching example

---
+++ pages/users/_id.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="user">
+    <h3>{{ name }}</h3>
+    <h4>@{{ username }}</h4>
+    <p>Email : {{ email }}</p>
+    <p><nuxt-link to="/users">List of users</nuxt-link></p>
+  </div>
+</template>
+
+<script>
+export default {
+  validate({ params }) {
+    return !isNaN(+params.id)
+  },
+  async asyncData({ app, params, error }) {
+    try {
+      const { data } = await app.$axios.get(`https://jsonplaceholder.typicode.com/users/${+params.id}`)
+      return data
+    } catch (e) {
+      error({ message: 'User not found', statusCode: 404 })
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '../../assets/styles/vars'
+
+  .user
+    text-align center
+    margin-top 100px
+    font-family sans-serif
+    h4
+      color $color_default
+</style>
